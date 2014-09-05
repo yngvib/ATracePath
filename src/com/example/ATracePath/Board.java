450627b8a45d18ea5b06a5e94fb97e6ac699b2c7
yngvib <yngvi@ru.is>
2014-09-05 13:58:55 +0000

    Initial commit

---
+++ src/com/example/ATracePath/Board.java
@@ -0,0 +1,79 @@
+package com.example.ATracePath;
+
+import android.content.Context;
+import android.graphics.*;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Board extends View {
+
+    private final int NUM_CELLS = 5;
+    private int m_cellWidth;
+    private int m_cellHeight;
+
+    private Rect m_rect = new Rect();
+    private Paint m_paintGrid  = new Paint();
+    private Paint m_paintPath  = new Paint();
+    private Path m_path = new Path();
+
+    private int xToCol( int x ) {
+        return (x - getPaddingLeft()) / m_cellWidth;
+    }
+
+    private int yToRow( int y ) {
+        return (y - getPaddingTop()) / m_cellHeight;
+    }
+
+    private int colToX( int col ) {
+        return col * m_cellWidth + getPaddingLeft() ;
+    }
+
+    private int rowToY( int row ) {
+        return row * m_cellHeight + getPaddingTop() ;
+    }
+
+    public Board(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        m_paintGrid.setStyle( Paint.Style.STROKE );
+        m_paintGrid.setColor( Color.GRAY );
+
+        m_paintPath.setStyle( Paint.Style.STROKE );
+        m_paintPath.setColor( Color.GREEN );
+        m_paintPath.setStrokeWidth( 8 );
+    }
+
+    @Override
+    protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
+        super.onMeasure( widthMeasureSpec, heightMeasureSpec );
+        int width  = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
+        int height = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
+        int size = Math.min(width, height);
+        setMeasuredDimension( size + getPaddingLeft() + getPaddingRight(),
+                size + getPaddingTop() + getPaddingBottom() );
+    }
+
+    @Override
+    protected void onSizeChanged( int xNew, int yNew, int xOld, int yOld ) {
+        int sw = Math.max(1, (int) m_paintGrid.getStrokeWidth());
+        m_cellWidth  = (xNew - getPaddingLeft() - getPaddingRight() - sw) / NUM_CELLS;
+        m_cellHeight = (yNew - getPaddingTop() - getPaddingBottom() - sw) / NUM_CELLS;
+    }
+
+    @Override
+    protected void onDraw( Canvas canvas ) {
+
+    }
+
+
+    @Override
+    public boolean onTouchEvent( MotionEvent event ) {
+
+        int x = (int) event.getX();         // NOTE: event.getHistorical... might be needed.
+        int y = (int) event.getY();
+
+        return true;
+    }
+
+}
