489cf53574a0480e2a1accda3d2ee1f9688a3117
yngvib <yngvi@ru.is>
2014-09-08 14:44:34 +0000

    Refactor activites.

---
+++ src/com/example/ATracePath/PlayActivity.java
@@ -0,0 +1,15 @@
+package com.example.ATracePath;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yngvi on 8.9.2014.
+ */
+public class PlayActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play);
+    }
+}
